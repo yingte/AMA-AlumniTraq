dd5fb7640b9bab2abcd39c6bebf323f3fc424771
Priscila Santos Romo <priscilasantos@tamu.edu>
2022-09-25 14:09:35 -0500

    add unit and integration testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_one :alumnus
   has_many :events, through: :event_attendees
   has_many :event_attendees
+
+  validates :first_name, :last_name, :major, :graduation_year, :email, :phone, presence: true
 end
