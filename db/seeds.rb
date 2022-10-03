e50b53787ec347097d1a51b9dd65316a765c0467
carolineemejia <hi123484@tamu.edu>
2022-10-03 14:09:29 -0500

    Adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ User.create!([
   {role_id: 1, first_name: "Alum", last_name: "One", major: "Computer Science", graduation_year: 2020, email: "alum.one@gmail.com", phone: "1111110000"},
   {role_id: 1, first_name: "Alum", last_name: "Two", major: "Computer Engineering", graduation_year: 2019, email: "alum.two@me.com", phone: "1111110001"},
   {role_id: 1, first_name: "Alum", last_name: "Three", major: "Poultry Science", graduation_year: 2018, email: "alum.three@comcast.net", phone: "1111110002"},
-  {role_id: 11, first_name: "Carolini", last_name: "Pal", major: "Finance", graduation_year: 2026, email: "cpal@tamu.edu", phone: "123444333"},
+  {role_id: 11, first_name: "Carolini", last_name: "Pal", major: "Finance", graduation_year: 2026, email: "cpal@tamu.edu", phone: "1234443333"},
 
 ])
 Alumnus.create!([
