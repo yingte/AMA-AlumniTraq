b013576403332841bace2025e630c64979477fde
Momo16136 <momo16136@tamu.edu>
2022-11-02 15:38:01 -0500

    Install bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap/dist/css"
+import "bootstrap/dist/js"
 
 Rails.start()
 Turbolinks.start()
