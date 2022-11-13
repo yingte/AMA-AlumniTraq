6d7840347c0a460e052a53e89f400e08291c5952
Luke Loera <lgloera2023@gmail.com>
2022-11-13 06:57:30 -0600

    Reorder loading of bootstrap.scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("jquery")
-require("jquery-ui")
 
 import "controllers"
 import Rails from "@rails/ujs"
@@ -14,7 +12,8 @@ import 'bootstrap/dist/js/bootstrap'
 import "bootstrap/dist/css/bootstrap";
 import "@fortawesome/fontawesome-free/css/all"
 
-
+require("jquery")
+require("jquery-ui")
 import $ from 'jquery'
 global.$ = jQuery
 Rails.start()
